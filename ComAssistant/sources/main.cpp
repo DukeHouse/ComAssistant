8414ca080c684bfb8b70603dce85273f9c48a8e7
inhowe <inhowe@qq.com>
2020-10-17 03:00:03 +0800

    [FEAT];增加qDebug定向到文件的功能

--- ComAssistant/sources/main.cpp
+++ ComAssistant/sources/main.cpp
@@ -1,10 +1,62 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QTranslator>
+#include <QDebug>
+
+void outputMessage(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    static QMutex mutex;
+    static int8_t first_run = 0;
+    mutex.lock();
+    QString text;
+    switch(type)
+    {
+    case QtDebugMsg:
+        text = QString("Debug:");
+        break;
+    case QtWarningMsg:
+        text = QString("Warning:");
+        break;
+    case QtCriticalMsg:
+        text = QString("Critical:");
+        break;
+    case QtFatalMsg:
+        text = QString("Fatal:");
+        break;
+    default:
+        text = QString("Debug:");
+        break;
+    }
+
+    QString context_info;
+    QString current_date_time;
+    QString current_date;
+//    context_info = QString("File:(%1) Line:(%2)").arg(QString(context.file)).arg(context.line);
+//    current_date_time = QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss ddd");
+//    current_date = QString("(%1)").arg(current_date_time);
+    QString message = QString("%1 %2 %3 %4").arg(text).arg(context_info).arg(msg).arg(current_date);
+
+    QFile file("ComAssistantDebug.txt");
+    file.open(QIODevice::WriteOnly | QIODevice::Append);
+    QTextStream text_stream(&file);
+    if(!first_run){
+        text_stream << endl << "----------New Debug File @" << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss ddd") << endl;
+        first_run = 1;
+    }
+    text_stream << message << endl;
+    file.flush();
+    file.close();
+    mutex.unlock();
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    //重定向qDebug到文件
+#ifdef QT_NO_DEBUG
+    qInstallMessageHandler(outputMessage);
+#endif
 
     //国际化
     QTranslator translator;
